39a7ff62d1b1e724b49a1a2e6ba3ddec2451d7c5
Jason Alberto <alberto.jason@gmail.com>
2023-04-10 22:45:48 -0400

    Complete dj challenge testing

--- dj/dj.js
+++ dj/dj.js
@@ -1,4 +1,41 @@
+function createSong(song, artist, hasBeenPlayed) {
+    return {
+        name: song,
+        artist: artist,
+        hasBeenPlayed: hasBeenPlayed || false 
+    }
+}
 
+function playSong(playedSong) {
+    if (!playedSong.hasBeenPlayed) {
+        playedSong.hasBeenPlayed = true;
+    }
+    return playedSong;
+}
 
+function makePlaylist(playlistName, playlistSong) {
+    return {
+        name: playlistName,
+        songs: playlistSong
+    }
+}
 
-module.exports = { };
+function addSongToPlaylist(playlist, addSong) {
+    playlist.songs.push(addSong);
+    return playlist;
+}
+
+function playAllSongs(playSongs) {
+    for (var i = 0; i < playSongs.songs.length; i++) {
+        playSong(playSongs.songs[i]);
+    }
+    return playSongs;
+}
+
+module.exports = {
+    createSong,
+    playSong,
+    makePlaylist,
+    addSongToPlaylist,
+    playAllSongs
+ };
